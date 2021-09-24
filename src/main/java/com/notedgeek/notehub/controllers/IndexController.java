5987ae6147a6f2ed7413f238da83abf453852ffd
seye <notedgeek@gmail.com>
2021-09-24 23:02:52 +0100

    try resource loading

--- src/main/java/com/notedgeek/notehub/controllers/IndexController.java
+++ src/main/java/com/notedgeek/notehub/controllers/IndexController.java
@@ -1,10 +1,16 @@
 package com.notedgeek.notehub.controllers;
 
 import com.notedgeek.notehub.asciidoctor.AsciidoctorConverter;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -23,11 +29,15 @@ public class IndexController {
 
     private synchronized void getHtml() {
         if(html == null) {
+            //String markup = Files.readString(Paths.get(ClassLoader.getSystemResource("classpath:index.adoc").toURI()));
             try {
-                String markup = Files.readString(Paths.get(ClassLoader.getSystemResource("classpath:index.adoc").toURI()));
+                Resource resource = new ClassPathResource("classpath:index.adoc");
+                InputStream inputStream = resource.getInputStream();
+                byte[] bytes = FileCopyUtils.copyToByteArray(inputStream);
+                String markup = new String(bytes, StandardCharsets.UTF_8);
                 html = asciidoctor.convert(markup);
-            } catch (Exception ex) {
-                ex.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
