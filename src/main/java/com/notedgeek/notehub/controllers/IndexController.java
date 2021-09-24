add4f462f95e58aae9fe5f707c5c6d401fe103c9
seye <notedgeek@gmail.com>
2021-09-24 23:25:33 +0100

    try resource loading

--- src/main/java/com/notedgeek/notehub/controllers/IndexController.java
+++ src/main/java/com/notedgeek/notehub/controllers/IndexController.java
@@ -1,20 +1,27 @@
 package com.notedgeek.notehub.controllers;
 
 import com.notedgeek.notehub.asciidoctor.AsciidoctorConverter;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.ResourceUtils;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Controller
 public class IndexController {
 
     private final AsciidoctorConverter asciidoctor = new AsciidoctorConverter();
     private String html = null;
+    private final ResourceLoader resourceLoader;
+
+    public IndexController(ResourceLoader resourceLoader) {
+        this.resourceLoader = resourceLoader;
+    }
 
     @GetMapping("/")
     public String index(Model model) {
@@ -25,10 +32,11 @@ public class IndexController {
 
     private synchronized void getHtml() {
         if(html == null) {
-            //String markup = Files.readString(Paths.get(ClassLoader.getSystemResource("classpath:index.adoc").toURI()));
             try {
-                File file = ResourceUtils.getFile("classpath:index.adoc");
-                String markup = new String(Files.readAllBytes(file.toPath()));
+                Resource resource = resourceLoader.getResource("classpath:index.adoc");
+                InputStream inputStream = resource.getInputStream();
+                byte[] bytes = FileCopyUtils.copyToByteArray(inputStream);
+                String markup = new String(bytes, StandardCharsets.UTF_8);
                 html = asciidoctor.convert(markup);
             } catch (IOException e) {
                 e.printStackTrace();
