80da83973a505839cb5eb187a57358a67048d76f
seye <notedgeek@gmail.com>
2021-09-25 02:45:59 +0100

    rendering

---
+++ src/main/java/com/notedgeek/notehub/controllers/rest/ConvertController.java
@@ -0,0 +1,21 @@
+package com.notedgeek.notehub.controllers.rest;
+
+import com.notedgeek.notehub.asciidoctor.AsciidoctorConverter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConvertController {
+
+    private final AsciidoctorConverter converter;
+
+    public ConvertController(AsciidoctorConverter converter) {
+        this.converter = converter;
+    }
+
+    @PostMapping("/convert")
+    public String convert(@RequestBody String adoc) {
+        return converter.convert(adoc);
+    }
+}
