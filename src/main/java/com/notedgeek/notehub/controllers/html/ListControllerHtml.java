d5a26601c30cd323f8a8a0760d5fd78698c53c78
seye <notedgeek@gmail.com>
2021-09-26 18:51:07 +0100

    tidy up

--- src/main/java/com/notedgeek/notehub/controllers/html/ListControllerHtml.java
+++ src/main/java/com/notedgeek/notehub/controllers/html/ListControllerHtml.java
@@ -1,16 +1,11 @@
 package com.notedgeek.notehub.controllers.html;
 
-import com.notedgeek.notehub.entity.Doc;
 import com.notedgeek.notehub.service.DocService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/list")
@@ -30,12 +25,7 @@ public class ListControllerHtml {
 
     @GetMapping("/{id}")
     public String list(@PathVariable long id, Model model) {
-        List<Doc> results = new ArrayList<>();
-        Optional<Doc> doc = service.findById(id);
-        if(doc.isPresent()) {
-            results.add(doc.get());
-        }
-        model.addAttribute("docs", results);
+        model.addAttribute("docs", service.findById(id).orElse(null));
         return "list";
     }
 
