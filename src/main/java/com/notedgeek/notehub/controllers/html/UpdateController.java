7a0b93ff36757b78e2c3c14a378e0b1c6b478b29
seye <notedgeek@gmail.com>
2021-09-27 15:34:55 +0100

    edit tags

--- src/main/java/com/notedgeek/notehub/controllers/html/UpdateController.java
+++ src/main/java/com/notedgeek/notehub/controllers/html/UpdateController.java
@@ -7,21 +7,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @RequestMapping("/update")
 public class UpdateController {
 
-    private final DocService service;
+    private final DocService docService;
 
-    public UpdateController(DocService service) {
-        this.service = service;
+    public UpdateController(DocService docService) {
+        this.docService = docService;
     }
 
     @PostMapping
-    public String update(@RequestParam long id, @RequestParam String markdown) {
-        Doc doc = service.findById(id).get();
+    public String update(@RequestParam long id, @RequestParam String markdown, @RequestParam String tags) {
+        Doc doc = docService.findById(id).get();
         doc.setMarkdown(markdown);
-        service.save(doc);
+        List<String> tagList = Arrays.asList(tags.split("[ ,]+"));
+        docService.setTags(doc, tagList);
+        docService.save(doc);
         return "redirect:/list";
     }
 
